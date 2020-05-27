29f57ff65c321c4b8b2ec735fed163af0145536c
J. Alvarez <jcalvarezj@gmail.com>
2020-05-27 00:00:38 -0500

    Improved with pylint suggestions

--- solutions/jcalvarezj/solution.py
+++ solutions/jcalvarezj/solution.py
@@ -1,8 +1,19 @@
+"""
+This module includes my solution to the challenge
+"""
 from typing import List
 
 
 class Solution:
+    """
+    This class represents a solution object that provides methods used to
+    compute a result
+    """
     def duplicate_zeros(self, arr: List[int]):
+        """
+        This method duplicates the zeros inside the input array, while
+        respecting its original size
+        """
         initial_size = len(arr)
         arr_enum = enumerate(arr.copy())
         acumulate = 0
